61b72ade61abcf2a373ccf43b5eb50ece6797645
João Costa <joaoarcosta@tecnico.ulisboa.pt>
2023-03-26 23:49:50 +0100

    feat: tortal inv price

--- fr/test.py
+++ fr/test.py
@@ -1,11 +1,10 @@
 import json
+from time import sleep
 import requests
-
 
 steamid = "76561198285623099"
 
 url = f"https://steamcommunity.com/profiles/{steamid}/inventory/json/730/2"
-
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
@@ -15,6 +14,7 @@ headers = {
     'Connection': 'keep-alive',
     'Cookie':'sessionid=e02317942bd01b0cc73283e8; timezoneOffset=0,0; cookieSettings={"version":1,"preference_state":2,"content_customization":null,"valve_analytics":null,"third_party_analytics":null,"third_party_content":null,"utm_enabled":true}; steamCountry=PT|7dce34e55c2187275d8dda742200b938; steamLoginSecure=76561198285623099||eyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MEQyMl8yMjQ3RjZFM19ERTFDOSIsICJzdWIiOiAiNzY1NjExOTgyODU2MjMwOTkiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY3OTg2ODIwOSwgIm5iZiI6IDE2NzExNDAwMDksICJpYXQiOiAxNjc5NzgwMDA5LCAianRpIjogIjBEMjdfMjI0N0Y3MEFfREEyMDIiLCAib2F0IjogMTY3OTc4MDAwOSwgInJ0X2V4cCI6IDE2OTc4Mzc0MDMsICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICI5NS45My4yNDIuMTQ0IiwgImlwX2NvbmZpcm1lciI6ICI5NS45My4yNDIuMTQ0IiB9.2H7Bjcn5I6wK12dHdNi-fKDM7J87xfBr-lkYMUfeBBbzJxtMm21xj3IiMLVHTa_XglX1ygjJSyYK7IztbYKNBg; browserid=2825485845585510381; webTradeEligibility={"allowed":1,"allowed_at_time":0,"steamguard_required_days":15,"new_device_cooldown_days":0,"time_checked":1679780044}; strInventoryLastContext=730_2'
 }
+
 
 def get_els():
     [print(i) for i in json.loads(requests.get(url, headers=headers).content)]
@@ -27,6 +27,7 @@ def import_items():
     file.write(json.dumps(descriptions, indent=4))
     file.close()
 
+
 def import_descs():
     response = requests.get(url, headers=headers)
     descriptions = json.loads(response.content)['rgDescriptions']
@@ -34,6 +35,16 @@ def import_descs():
     file = open("descs.json",'w',encoding='utf-8')
     file.write(json.dumps(descriptions, indent=4))
     file.close()
+
+
+def filter(dic:dict, func):
+    # filter the dict by the function
+    return {k: v for k, v in dic.items() if func(v)}
+
+
+def sort(dic:dict, func):
+    #TODO sort the dict by the function
+    return
 
 
 def get_qnts_by_items():
@@ -44,21 +55,55 @@ def get_qnts_by_items():
     descs = json.load(descs)
     for i in descs:
         values = descs[i]
-        dic[i] = 0
+        if values["marketable"] == 1:
+            temp = {
+                "quantity": 0,
+                "name" : values["market_hash_name"],
+                # "name_color" : "#" + values["name_color"],
+                "type": values["type"],
+                "price": 0.00
+            }
+            dic[i] = temp
     
 
     qnts = open("qnts.json",'r',encoding='utf-8')
     qnts = json.load(qnts)
     for i in qnts:
         values = qnts[i]
-        dic[values["classid"] + "_"+ values["instanceid"]] += 1
-    
+        try:
+            dic[values["classid"] + "_"+ values["instanceid"]]["quantity"] += 1
+        except:
+            pass
+
+
+    #TODO sort the dict by quantity
+    dic = dict(sorted(dic.items(), key=lambda item: item[1]["quantity"], reverse=True))
+
+    total = 0.0
+    loop = 0
     for i in dic:
-        if dic[i] > 2:
-            print(i, dic[i])
-    # print(dic)
+        while True:
+            try:
+                price_url = f'https://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name={dic[i]["name"]}'
+                response = requests.get(price_url, headers=headers)
+                price = json.loads(response.content)['lowest_price'][:-1].replace(",",".")
+                print(dic[i]["quantity"], dic[i]["name"], price+"€")
+                total += dic[i]["quantity"] * float(price)
+                dic[i]["price"] = float(price)
+                break
+            except:
+                print(response.content)
+                sleep(60)
+        loop -= 1
+        if loop == 0:
+            break
 
+    print("total" , round(total, 2) , "€")
 
+    file = open("inv.json",'w',encoding='utf-8')
+    file.write(json.dumps(dic, indent=4))
+    file.write('\n"total": ' + (str)(round(total, 2)) + "€\n")
+    file.close()
 
 
 # get_els()
