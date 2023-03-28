9bb6857334a312c522b9fc1cf38b12957f98f527
João Costa <joaoarcosta@tecnico.ulisboa.pt>
2023-03-28 23:09:15 +0100

    switched server and serverImpl and some stuff

--- server/domain/inventory/inventory.py
+++ server/domain/inventory/inventory.py
@@ -9,17 +9,9 @@ from domain.inventory import inventoryImpl
 
 from utils.dbConnection import get_db_connection_string
 from utils.log import log_join
-import serverImpl
-
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://steamcommunity.com/',
-    'Connection': 'keep-alive',
-    # TODO in server: get cookies from browser
-    'Cookie':'''sessionid=e02317942bd01b0cc73283e8; cookieSettings={"version":1,"preference_state":2,"content_customization":null,"valve_analytics":null,"third_party_analytics":null,"third_party_content":null,"utm_enabled":true}; browserid=2825485845585510381; webTradeEligibility={"allowed":1,"allowed_at_time":0,"steamguard_required_days":15,"new_device_cooldown_days":0,"time_checked":1679780044}; strInventoryLastContext=730_2; timezoneOffset=3600,0; steamDidLoginRefresh=1680024977; steamCountry=PT|7622fb70dbc7c719ac7f7e9a46043d42; steamLoginSecure=76561198285623099||eyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MEQyMl8yMjQ3RjZFM19ERTFDOSIsICJzdWIiOiAiNzY1NjExOTgyODU2MjMwOTkiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY4MDExMjYyNiwgIm5iZiI6IDE2NzEzODQ5ODEsICJpYXQiOiAxNjgwMDI0OTgxLCAianRpIjogIjBEMjdfMjI0QkQyM0ZfRjI0MjMiLCAib2F0IjogMTY3OTc4MDAwOSwgInJ0X2V4cCI6IDE2OTc4Mzc0MDMsICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICI5NS45My4yNDIuMTQ0IiwgImlwX2NvbmZpcm1lciI6ICI5NS45My4yNDIuMTQ0IiB9.4_maWs5Yzrb7Sf-ZoUd0wbRmH2kkDBqfWl1_2DHGazNZSBUvhZhHkUwOS1GJts5nss-2meWac8lv_Z2NcJ6_BQ'''
-}
+import server.server as server
+import domain
+from utils.headers import HEADERS
 
 
 def update_inv():
@@ -29,7 +21,7 @@ def update_inv():
 
 def get_page():
     data = []
-    for i in serverImpl.get_steam_profiles():
+    for i in server.get_steam_profiles():
         data.append(i)
 
     return render_template("inventory/steamids.html", title="Inventory", cursor=data)
@@ -37,38 +29,40 @@ def get_page():
 def get_inventory_for_steamid(steamid):
     inv = inventoryImpl.get_inventory(steamid)
    
+    prices = domain.prices.get_prices()
+
     data = []
     total_price = 0
     quantity = 0
     for i in inv:
         data.append(inv[i])
         quantity += inv[i]["quantity"]
-        total_price += inv[i]["total price"]
+        total_price += round(prices[i]["price"] * inv[i]["quantity"], 2)
     data.append({"name": "Total", "quantity": quantity, "total price": round(total_price,2), "price": round(total_price/quantity, 2)})
     return render_template("inventory/inventory.html", title="Inventory", cursor=data)
 
-def update(steamid):
-    inventory_url = f"https://steamcommunity.com/profiles/{steamid}/inventory/json/730/2"
-
-    response = requests.get(inventory_url, headers=HEADERS)
-    content = json.loads(response.content)
-    inventory, descriptions = content['rgInventory'], content['rgDescriptions']
-
-    inv = inventoryImpl.json_to_inv(inventory, descriptions)
-
-    inventoryImpl.save_inv(steamid, inv)
-
-    return render_template("redirect_to_root.html", title="Update Prices")
-
-
 def update(steamid, js):
+    if js == None:
+        inventory_url = f"https://steamcommunity.com/profiles/{steamid}/inventory/json/730/2"
+        js = requests.get(inventory_url, headers=HEADERS).content
+
     content = json.loads(js)
     inventory, descriptions = content['rgInventory'], content['rgDescriptions']
-    
+        
     inv = inventoryImpl.json_to_inv(inventory, descriptions)
 
     inventoryImpl.save_inv(steamid, inv)
 
     return render_template("redirect_to_root.html", title="Update Prices")
+
+
+
+
+
+
+
+
+
+
 
 
--- server/domain/prices/prices.py
+++ server/domain/prices/prices.py
@@ -1,43 +1,95 @@
+from datetime import datetime
 import json
 import os
+from alive_progress import alive_bar
 from flask import render_template, session
 import psycopg2
 from psycopg2.extras import DictCursor
+import requests
 from domain.inventory import inventoryImpl
+from time import sleep
+import time
 
 from utils.dbConnection import get_db_connection_string
 from utils.log import log_join
-import serverImpl
+from utils.headers import HEADERS
+import server.server as server
 
-def load_prices():
-    inv_map = {}
-    for steamid in serverImpl.get_steam_ids():
+def get_prices():
+    dir_path = './saves/prices'
+    files = sorted(os.listdir(dir_path))
+    files.reverse()
+    try:
+        print("loading last prices:", files[0])
+        file = open(dir_path + "/" + files[0], 'r',encoding='utf-8')
+        prices = json.load(file)
+        file.close()
+        return dict(prices)
+    except IndexError:
+        # print("no prices found")
+        return {}
+
+
+def load_items():
+    item_map = {}
+    for steamid in server.get_steam_ids():
         inv = inventoryImpl.get_inventory(steamid)
-        
         for index in inv:
             try:
-                inv_map[inv[index]["name"]]["quantity"] += inv[index]["quantity"]
+                item_map[inv[index]["name"]]["quantity"] += inv[index]["quantity"]
             except:
-                inv_map[inv[index]["name"]] = inv[index]
+                item_map[inv[index]["name"]] = inv[index]
     
-    return inv_map # has all the items to be updated
+    item_map = dict(sorted(inv.items(), key=lambda item: item[1]["quantity"], reverse=True))
+    return item_map # has all the items to be updated
 
 
 def get_page():
     data = []
-    dic = load_prices()
-    inv_by_total_price = dict(sorted(dic.items(), key=lambda item: item[1]["total price"], reverse=True))
-    inv_by_quantity = dict(sorted(dic.items(), key=lambda item: item[1]["quantity"], reverse=True))
+    dic = load_items()
+    # inv = dict(sorted(dic.items(), key=lambda item: item[1]["total price"], reverse=True))
+    inv = dict(sorted(dic.items(), key=lambda item: item[1]["quantity"], reverse=True))
     
-    inv = inv_by_total_price
     for i in inv:
         data.append(inv[i])
     return render_template("prices/prices.html", title="Prices", cursor=data)
 
 
 def update():
-    inv_map = load_prices()
+    item_map = load_items()
+    try:
+        del item_map['quantity']
+        del item_map['total price']
+    except:
+        pass
+    
+    for item in item_map:
+        while True:
+            try:
+                price_url = f'https://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name={item_map[item]["name"]}'
+                response = requests.get(price_url, headers=HEADERS)
+                print(response.text, item["name"])
+                lowest_price = float(json.loads(response.content)['lowest_price'][:-1].replace(",","."))
+                # print("updating", item_map[item]["name"], "new price", lowest_price)
+                item_map[item]["price"] = lowest_price
+                break
+            except TypeError as e:
+                print(price_url)
+                with alive_bar(600) as bar:
+                    for _ in range(600):
+                        sleep(0.1)
+                        bar()
+            except KeyError:
+                print("KeyError", item_map[item]["name"], "\n", price_url)
+
+
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    file_name =f"saves/prices/{time}.json"
+
+    print("saving :", file_name)
+    with open(file_name,'w',encoding='utf-8') as file:
+        file.write(json.dumps(item_map, indent=4))
+        file.close()
+
     #TODO: update prices in db
     return render_template("redirect_to_root.html", title="Update Prices")
-
-
