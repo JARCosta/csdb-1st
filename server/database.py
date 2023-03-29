9cc4018c3f2154ea528c6da0ed24ad8136a984eb
João Costa <joaoarcosta@tecnico.ulisboa.pt>
2023-03-29 17:51:17 +0100

    database populate v1

--- server/database.py
+++ server/database.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2.extras import DictCursor
+import server
 
 import utils
 
@@ -25,6 +26,29 @@ def add_item(name: str, type: str):
         cursor = dbConn.cursor(cursor_factory=DictCursor)
         cursor.execute(f"INSERT INTO items (name, type) VALUES ('{name}', '{type}');")
         print(f"added item {name} with type {type}")
+    finally:
+        dbConn.commit()
+        cursor.close()
+        dbConn.close()
+
+
+def add_to_inventory(steamid: str, item_name: str, quantity: int):
+    try:
+        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        cursor = dbConn.cursor(cursor_factory=DictCursor)
+        cursor.execute(f"INSERT INTO profile_items (profile, item, quantity) VALUES ('{steamid}', '{item_name}','{quantity}');")
+        print(f"added item {item_name} to inventory of {steamid}")
+    finally:
+        dbConn.commit()
+        cursor.close()
+        dbConn.close()
+
+def add_item_price(item_name: str, price: int, date: str):
+    try:
+        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        cursor = dbConn.cursor(cursor_factory=DictCursor)
+        cursor.execute(f"INSERT INTO item_prices (item, price) VALUES ('{item_name}', '{price}');")
+        print(f"added price {price} to item {item_name}")
     finally:
         dbConn.commit()
         cursor.close()
